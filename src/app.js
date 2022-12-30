b822c6318dc91bc77e45e34dea3909143de1b2f0
marynmcnamara <119798224+marynmcnamara@users.noreply.github.com>
2022-12-30 16:41:08 -0500

    added form and java script

--- src/app.js
+++ src/app.js
@@ -41,8 +41,18 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+function search(city) {
+  let apiKey = "63cd308f75720f22d3aeffe4bbba1f05";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-let city = "Lisbon";
-let apiKey = "63cd308f75720f22d3aeffe4bbba1f05";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Boston");
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
